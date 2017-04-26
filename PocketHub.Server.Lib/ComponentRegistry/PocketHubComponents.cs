1909bba238c92bc5077f381d761e2beab813e398
Pete @BK1 Svr <petersonsalamat@gmail.com>
2017-04-26 18:01:43 +0800

    Backup before biyahe.

--- PocketHub.Server.Lib/ComponentRegistry/PocketHubComponents.cs
+++ PocketHub.Server.Lib/ComponentRegistry/PocketHubComponents.cs
@@ -1,7 +1,11 @@
 ﻿using Autofac;
+using PocketHub.Server.Lib.Configuration;
 using PocketHub.Server.Lib.Logging;
 using PocketHub.Server.Lib.MainTabs.ConnectionsTab;
+using Repo2.Core.ns11.Authentication;
 using Repo2.SDK.WPF45.Extensions.IOCExtensions;
+using Repo2.SDK.WPF45.Extensions.ViewModelExtensions;
+using System.Windows;
 
 namespace PocketHub.Server.Lib.ComponentRegistry
 {
@@ -11,12 +15,21 @@ namespace PocketHub.Server.Lib.ComponentRegistry
         {
             //b.Solo<ConnectionsTabVM>();
             //b.Solo<ServerToggleVM>();
-
             b.RegisterGeneric(typeof(ConnectionsTabVM<>)).AsSelf().SingleInstance();
             b.RegisterGeneric(typeof(ServerToggleVM<>)).AsSelf().SingleInstance();
 
             b.Solo<CurrentClientsVM>();
             b.Solo<ActivityLogVM>();
+
+            var cfg = ServerSettings.LoadFile();
+            b.RegisterInstance(cfg).As<IR2Credentials>()
+                                   .AsSelf();
+        }
+
+
+        public static void SetCommonTemplates(Application app)
+        {
+            app.SetTemplate<ConnectionsTabVM<>, ConnectionsTabUI>();
         }
     }
 }
