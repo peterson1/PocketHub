76f64422f61a82979f8a153a257923e098f8f7dd
Peterson Salamat <petersonsalamat@gmail.com>
2017-05-08 03:39:36 +0800

    Extracted out a base class from RestServerTokenAuthAttribute in preparation for other AuthAttributes.

---
+++ PocketHub.Server.Lib/Authentication/HeaderTokensAuthAttributeBase.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using PocketHub.Client.Lib.UserInterfaces.Logging;
+using PocketHub.Server.Lib.ComponentRegistry;
+using Repo2.Core.ns11.Exceptions;
+using Repo2.Core.ns11.Extensions.StringExtensions;
+using System;
+using System.Threading.Tasks;
+
+namespace PocketHub.Server.Lib.Authentication
+{
+    public abstract class HeaderTokensAuthAttributeBase : AuthorizeAttribute
+    {
+        protected ActivityLogVM _log;
+
+        public HeaderTokensAuthAttributeBase(ActivityLogVM activityLogVM)
+        {
+            _log = activityLogVM;
+        }
+
+
+        protected abstract Task<bool> CanConnect(string username, string authToken);
+        protected abstract bool       CanInvoke (string username, string connectionId, string hubName, string methodName);
+
+
+        public override bool AuthorizeHubConnection(HubDescriptor hubDescriptor, IRequest request)
+        {
+            if (!request.IsNegotiating()) return true;
+
+            var userNme  = request.GetUserName();
+            if (userNme.IsBlank()) return false;
+
+            var authTokn = request.GetAuthToken();
+            if (authTokn.IsBlank()) return false;
+
+            _log.Info($"Authenticating as “{userNme}” ...");
+
+            var ok = false;
+            try
+            {
+                ok = CanConnect(userNme, authTokn).ConfigureAwait(false).GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                _log.Info(ex.Info(false, false));
+                //return false;  --> client see 401:Unauthorized whatever the error is
+                throw ex;     // --> client sees actual HTTP error code
+            }
+
+            if (!ok) _log.Info("Access denied.");
+            return ok;
+        }
+
+
+        public override bool AuthorizeHubMethodInvocation(IHubIncomingInvokerContext invoker, bool appliesToMethod)
+        {
+            var usrNme = invoker.Hub.Context.Request.GetUserName();
+            if (usrNme.IsBlank()) return false;
+
+            var connId = invoker.Hub.Context.ConnectionId;
+            var method = invoker.MethodDescriptor;
+
+            return CanInvoke(usrNme, connId, method.Hub.Name, method.Name);
+        }
+    }
+}
--- PocketHub.Server.Lib/Authentication/RestServerTokenAuthAttribute.cs
+++ PocketHub.Server.Lib/Authentication/RestServerTokenAuthAttribute.cs
@@ -1,65 +1,64 @@
-﻿using Microsoft.AspNet.SignalR;
-using Microsoft.AspNet.SignalR.Hubs;
-using PocketHub.Client.Lib.UserInterfaces.Logging;
+﻿using PocketHub.Client.Lib.UserInterfaces.Logging;
 using PocketHub.Server.Lib.ComponentRegistry;
-using Repo2.Core.ns11.Authentication;
-using Repo2.Core.ns11.Exceptions;
-using Repo2.Core.ns11.Extensions.StringExtensions;
-using System;
+using System.Threading.Tasks;
 
 namespace PocketHub.Server.Lib.Authentication
 {
-    public class RestServerTokenAuthAttribute : AuthorizeAttribute
+    public class RestServerTokenAuthAttribute : HeaderTokensAuthAttributeBase
     {
-        private ActivityLogVM           _log;
-        private IR2Credentials          _creds;
         private AuthServerTokenChecker  _authSvr;
 
 
         public RestServerTokenAuthAttribute()
+            : base(ComponentRegistryBase.Resolve<ActivityLogVM>())
         {
-            _log     = ComponentRegistryBase.Resolve<ActivityLogVM>();
-            _creds   = ComponentRegistryBase.Resolve<IR2Credentials>();
             _authSvr = ComponentRegistryBase.Resolve<AuthServerTokenChecker>();
         }
 
-
-        public override bool AuthorizeHubConnection(HubDescriptor hubDescriptor, IRequest request)
-        {
-            if (!request.IsNegotiating()) return true;
-
-            var userNme  = request.GetUserName();
-            if (userNme.IsBlank()) return false;
-
-            var authTokn = request.GetAuthToken();
-            if (authTokn.IsBlank()) return false;
-
-            _log.Info($"Authenticating on [{_creds.BaseURL}] as “{userNme}” ...");
+        protected override Task<bool> CanConnect(string username, string authToken)
+            => _authSvr.IsAuthorized(username, authToken);
 
 
-            var ok = false;
-            try
-            {
-                ok = _authSvr.IsAuthorized(userNme, authTokn).ConfigureAwait(false).GetAwaiter().GetResult();
-            }
-            catch (Exception ex)
-            {
-                _log.Info(ex.Info(false, false));
-                //return false;  --> client see 401:Unauthorized whatever the error is
-                throw ex;     // --> client sees actual HTTP error code
-            }
-
-            if (!ok) _log.Info("Access denied.");
-            return ok;
-        }
+        protected override bool CanInvoke(string username, string connectionId, string hubName, string methodName)
+            => _authSvr.HasProfile(username);
 
 
-        public override bool AuthorizeHubMethodInvocation(IHubIncomingInvokerContext hubIncomingInvokerContext, bool appliesToMethod)
-        {
-            var usrNme = hubIncomingInvokerContext.Hub.Context.Request.GetUserName();
-            if (usrNme.IsBlank()) return false;
+        //public override bool AuthorizeHubConnection(HubDescriptor hubDescriptor, IRequest request)
+        //{
+        //    if (!request.IsNegotiating()) return true;
 
-            return _authSvr.HasProfile(usrNme);
-        }
+        //    var userNme  = request.GetUserName();
+        //    if (userNme.IsBlank()) return false;
+
+        //    var authTokn = request.GetAuthToken();
+        //    if (authTokn.IsBlank()) return false;
+
+        //    _log.Info($"Authenticating on [{_creds.BaseURL}] as “{userNme}” ...");
+
+
+        //    var ok = false;
+        //    try
+        //    {
+        //        ok = _authSvr.IsAuthorized(userNme, authTokn).ConfigureAwait(false).GetAwaiter().GetResult();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _log.Info(ex.Info(false, false));
+        //        //return false;  --> client see 401:Unauthorized whatever the error is
+        //        throw ex;     // --> client sees actual HTTP error code
+        //    }
+
+        //    if (!ok) _log.Info("Access denied.");
+        //    return ok;
+        //}
+
+
+        //public override bool AuthorizeHubMethodInvocation(IHubIncomingInvokerContext hubIncomingInvokerContext, bool appliesToMethod)
+        //{
+        //    var usrNme = hubIncomingInvokerContext.Hub.Context.Request.GetUserName();
+        //    if (usrNme.IsBlank()) return false;
+
+        //    return _authSvr.HasProfile(usrNme);
+        //}
     }
 }
