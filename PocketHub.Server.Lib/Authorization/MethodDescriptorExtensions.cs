ffb9ae2c981bd097055f1f7dc7cd2bb8ce59b139
Pete @BK1 Svr <petersonsalamat@gmail.com>
2017-05-10 11:02:16 +0800

    Removed superfluous type argument.

--- PocketHub.Server.Lib/Authorization/MethodDescriptorExtensions.cs
+++ PocketHub.Server.Lib/Authorization/MethodDescriptorExtensions.cs
@@ -8,14 +8,13 @@ namespace PocketHub.Server.Lib.Authorization
     public static class MethodDescriptorExtensions
     {
 
-        public static List<string> GetRoles<T>(this MethodDescriptor hubMethod)
-            where T : AuthorizeAttribute
+        public static List<string> GetRoles(this MethodDescriptor hubMethod)
         {
             var roles = new List<string>();
 
             foreach (var attr in hubMethod.Attributes)
             {
-                var authAttr = attr as T;
+                var authAttr = attr as AuthorizeAttribute;
                 if (authAttr == null) continue;
                 if (authAttr.Roles.IsBlank()) continue;
                 roles.AddRange(authAttr.Roles.SplitTrim(","));
