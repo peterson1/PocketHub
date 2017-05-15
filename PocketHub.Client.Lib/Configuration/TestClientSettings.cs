21a06f8f077245af3d72ddaf8868d673cd84b14a
Pete @BK1 Svr <petersonsalamat@gmail.com>
2017-05-15 14:08:08 +0800

    Added configuration file for use in test projects.

---
+++ PocketHub.Client.Lib/Configuration/TestClientSettings.cs
@@ -0,0 +1,29 @@
+﻿using Repo2.SDK.WPF45.Configuration;
+using Repo2.SDK.WPF45.FileSystems;
+
+namespace PocketHub.Client.Lib.Configuration
+{
+    public class TestClientSettings : ClientSettings
+    {
+        public string  TestLogin     { get; set; }
+        public string  TestPassword  { get; set; }
+
+
+        public static TestClientSettings GetDefault()
+            => new TestClientSettings
+            {
+                HubServerURL = "http://localhost:33301",
+                SharedKey    = "????",
+                TestLogin    = "????",
+                TestPassword = "????",
+            };
+
+
+        public static TestClientSettings ReadFile()
+        {
+            var fs    = new FileSystemAccesor1();
+            var loadr = new BesideExeCfgLoader<TestClientSettings>(fs);
+            return loadr.Load(TestClientSettings.GetDefault());
+        }
+    }
+}
