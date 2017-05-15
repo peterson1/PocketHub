3c47c5b3b38baa39c37fb0ef10021cda377596cd
Pete @BK1 Svr <petersonsalamat@gmail.com>
2017-05-15 14:21:23 +0800

    Enabled component registries to accept null app parameters -- intended for use by test cases.

--- PocketHub.Server.Lib/ComponentRegistry/ComponentRegistryBase.cs
+++ PocketHub.Server.Lib/ComponentRegistry/ComponentRegistryBase.cs
@@ -1,13 +1,11 @@
 ﻿using Autofac;
 using Autofac.Core;
 using PocketHub.Client.Lib.UserInterfaces.Logging;
-using PocketHub.Server.Lib.Authentication;
 using PocketHub.Server.Lib.Authorization;
 using PocketHub.Server.Lib.Configuration;
 using PocketHub.Server.Lib.MainTabs.ConnectionsTab;
 using PocketHub.Server.Lib.SignalRHubs;
 using PocketHub.Server.Lib.UserAccounts;
-using Repo2.Core.ns11.Authentication;
 using Repo2.Core.ns11.Exceptions;
 using Repo2.SDK.WPF45.ComponentRegistry;
 using Repo2.SDK.WPF45.Exceptions;
@@ -22,9 +20,11 @@ namespace PocketHub.Server.Lib.ComponentRegistry
     {
         public ComponentRegistryBase(Application app)
         {
-            app.SetTemplate<ConnectionsTabVM<T>, ConnectionsTabUI>();
-
-            SetDataTemplates(app);
+            if (app != null)
+            {
+                app.SetTemplate<ConnectionsTabVM<T>, ConnectionsTabUI>();
+                SetDataTemplates(app);
+            }
 
             var containr = RegisterAllComponents();
             ComponentRegistryBase.BeginLifetimeScope(containr);
